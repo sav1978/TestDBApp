b9505176afa437763229f8618e73b7c239e6ed82
SAV <semkovav@gmail.com>
2017-10-09 01:40:45 +0300

    Initial local commit

---
+++ TestDBApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDBApp
+{
+    public partial class Form1 : Form
+    {
+        private DataGridView dataGridView1 = new DataGridView();
+        private BindingSource bindingSource1 = new BindingSource();
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+        private Button reloadButton = new Button();
+        private Button submitButton = new Button();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize the form.
+            this.dataGridView1.Dock = DockStyle.Fill;
+            reloadButton.Text = "reload";
+            submitButton.Text = "submit";
+            reloadButton.Click += new System.EventHandler(reloadButton_Click);
+            submitButton.Click += new System.EventHandler(submitButton_Click);
+
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+            panel.Dock = DockStyle.Top;
+            panel.AutoSize = true;
+            panel.Controls.AddRange(new Control[] { reloadButton, submitButton });
+
+            this.Controls.AddRange(new Control[] { dataGridView1, panel });
+            this.Load += new System.EventHandler(Form1_Load);
+            this.Text = "DataGridView databinding and updating demo";
+        }
+
+        private void reloadButton_Click(object sender, System.EventArgs e)
+        {
+            // Reload the data from the database.
+            GetData(dataAdapter.SelectCommand.CommandText);
+        }
+
+        private void submitButton_Click(object sender, System.EventArgs e)
+        {
+            // Update the database with the user's changes.
+            dataAdapter.Update((DataTable)bindingSource1.DataSource);
+        }
+
+
+        private DataTable GetData(string selectCommand)
+        {
+            string connectionString =
+                @"Data Source=(localdb)\Projects;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+
+            // Connect to the database and fill a data table.
+            dataAdapter =
+                new SqlDataAdapter(selectCommand, connectionString);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+            DataTable data = new DataTable();
+            data.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            dataAdapter.Fill(data);
+
+            return data;
+        }
+
+        private void Form1_Load(System.Object sender, System.EventArgs e)
+        {
+            // Attach DataGridView events to the corresponding event handlers.
+            this.dataGridView1.CellValidating += new
+                DataGridViewCellValidatingEventHandler(dataGridView1_CellValidating);
+            this.dataGridView1.CellEndEdit += new
+                DataGridViewCellEventHandler(dataGridView1_CellEndEdit);
+
+            // Initialize the BindingSource and bind the DataGridView to it.
+            bindingSource1.DataSource = GetData("select * from Customers");
+            this.dataGridView1.DataSource = bindingSource1;
+            this.dataGridView1.AutoResizeColumns(
+                DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            string headerText =
+                dataGridView1.Columns[e.ColumnIndex].HeaderText;
+
+            // Abort validation if cell is not in the CompanyName column.
+            if (!headerText.Equals("CompanyName")) return;
+
+            // Confirm that the cell is not empty.
+            if (string.IsNullOrEmpty(e.FormattedValue.ToString()))
+            {
+                dataGridView1.Rows[e.RowIndex].ErrorText =
+                    "Company Name must not be empty";
+                e.Cancel = true;
+            }
+        }
+
+        void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            // Clear the row error in case the user presses ESC.   
+            dataGridView1.Rows[e.RowIndex].ErrorText = String.Empty;
+        }
+    }
+}
